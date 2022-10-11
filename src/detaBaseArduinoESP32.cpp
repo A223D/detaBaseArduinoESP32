53958b6fcfe26cd09d1422e50618ac13f9e4d2ae
Mikdan Jey <mikdanjey@gmail.com>
2022-10-12 00:12:33 +0530

    fixed bug WiFiClientSecure has no member setCACert

--- src/detaBaseArduinoESP32.cpp
+++ src/detaBaseArduinoESP32.cpp
@@ -7,7 +7,9 @@ DetaBaseObject::DetaBaseObject(WiFiClientSecure wifiObject, const char* detaID, 
   _detaBaseName = detaBaseName;
   _baseURI = (char *)malloc((strlen("/v1/") + strlen(detaID) + strlen("/") + strlen(detaBaseName) + 1 ) * sizeof(char));
   _wifiObject = wifiObject;
-  _wifiObject.setCACert(_detaRootCa);
+  // _wifiObject.setCACert(_detaRootCa);
+  _wifiObject.setFingerprint(fingerprint);
+  _wifiObject.setTimeout(15000);  // 15 Seconds
   _host = "database.deta.sh";
   strcpy(_baseURI, "/v1/");
   strncat(_baseURI, detaID, strlen(detaID));
@@ -21,7 +23,9 @@ DetaBaseObject::DetaBaseObject(WiFiClientSecure wifiObject, const char* detaID, 
   _detaBaseName = detaBaseName;
   _baseURI = (char *)malloc((strlen("/v1/") + strlen(detaID) + strlen("/") + strlen(detaBaseName) + 1 ) * sizeof(char));
   _wifiObject = wifiObject;
-  _wifiObject.setCACert(_detaRootCa);
+  // _wifiObject.setCACert(_detaRootCa);
+  _wifiObject.setFingerprint(fingerprint);
+  _wifiObject.setTimeout(15000);  // 15 Seconds
   _host = "database.deta.sh";
   strcpy(_baseURI, "/v1/");
   strncat(_baseURI, detaID, strlen(detaID));
